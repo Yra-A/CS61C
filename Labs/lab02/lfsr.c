6a22b83d156b5070bd8253d91b86b145a388e1bc
Yra-A <1325083019@qq.com>
2023-04-15 12:28:42 +0800

    lab2 ex2 finish

--- Labs/lab02/lfsr.c
+++ Labs/lab02/lfsr.c
@@ -6,5 +6,12 @@
 
 void lfsr_calculate(uint16_t *reg) {
     /* YOUR CODE HERE */
+    uint16_t v = (*reg & 1) ^ (*reg >> 2 & 1) ^ (*reg >> 3 & 1) ^ (*reg >> 5 & 1);
+    *reg >>= 1;
+    if (v) {
+        *reg |= (1 << 15);
+    } else {
+        *reg &= ~(1 << 15);
+    }
 }
 
