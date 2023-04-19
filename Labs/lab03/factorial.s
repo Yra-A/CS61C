ebca40403cebf15de246bf59ce9af3720893d3a0
Yra-A <1325083019@qq.com>
2023-04-19 09:56:54 +0800

    lab3 ex3 finish

--- Labs/lab03/factorial.s
+++ Labs/lab03/factorial.s
@@ -6,8 +6,8 @@ n: .word 8
 .text
 main:
     la t0, n
-    lw a0, 0(t0)
-    jal ra, factorial
+    lw a0, 0(t0) # a0 = n;
+    jal ra, factorial 
 
     addi a1, a0, 0
     addi a0, x0, 1
@@ -22,3 +22,13 @@ main:
 
 factorial:
     # YOUR CODE HERE
+    li t0, 1
+    add t1, x0, a0
+loop:
+    beq t1, x0, end
+    mul t0, t0, t1
+    addi t1, t1, -1
+    j loop
+end:
+    add a0, x0, t0
+    jr ra
